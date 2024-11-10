6ac5eba5aba73adce1ebce620a5bcef690731c38
Megan Nowik <megannowik@gmail.com>
2024-11-10 17:19:53 -0500

    Add files via upload

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,9 +13,9 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
+        transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * 10f);
 
-        if (transform.position.y < -8f)
+        if (transform.position.y > 8f)
         {
             Destroy(this.gameObject);
         }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,71 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-
 
 public class Player : MonoBehaviour
 {
+    //variables
+    //1. access level: public or private
+    //2. type: int (e.g., 2, 4, 123, 3456, etc.), float (e.g, 2.5, 3.67, etc.)
+    //3. name: (1) start w/ lowercase (2) if it is multiple words, then the other words start with uppercase and written together
+    //4. optional: give it an initial value
 
     private float horizontalInput;
     private float verticalInput;
     private float speed;
-    private float horizontalScreenLimit;
-    private float verticalScreenLimit;
+    private int lives;
+    float MaxdistanceY = 0f;
+    float MindistanceY = -4f;
 
-    public GameObject explosion;
     public GameObject bullet;
-    public int lives;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 6f;
-        horizontalScreenLimit = 11.5f;
-        verticalScreenLimit = 7.5f;
         lives = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
+        Moving();
         Shooting();
     }
 
-    void Movement()
+    void Moving()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * speed);
-        if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
+
+        //if my x position is bigger than 11.5f than I am outside the screen from the right 
+        if (transform.position.x > 11.5f || transform.position.x <= -11.5f)
         {
             transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
         }
-        if (transform.position.y > verticalScreenLimit || transform.position.y <= -verticalScreenLimit)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
-        }
+
+        float clampedY = Mathf.Clamp(transform.position.y, MindistanceY, MaxdistanceY);
+        transform.position = new Vector3(transform.position.x, clampedY, 0);
     }
 
     void Shooting()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        //if SPACE key is pressed create a bullet; what is a bullet?
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+            //create a bullet object at my position with my rotation
+            Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity); 
         }
-    }
-
-    public void LoseALife()
-    {
-        //lives = lives - 1;
-        //lives -= 1;
-        lives--;
-        if (lives == 0)
-        {
-            Instantiate(explosion, transform.position, Quaternion.identity);
-            Destroy(this.gameObject);
-        }
-        GameObject.Find("GameManager").GetComponent<GameManager>().LivesCounter(lives);
     }
 }
